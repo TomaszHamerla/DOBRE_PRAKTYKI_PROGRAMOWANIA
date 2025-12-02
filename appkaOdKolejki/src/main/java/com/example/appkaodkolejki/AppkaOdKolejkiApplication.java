a7d8744bec35ed75918a7411ce2ecd10cb3e4871
unknown <tomaszhamerla@interia.pl>
2025-12-02 19:17:10 +0100

    init apki v2

---
+++ appkaOdKolejki/src/main/java/com/example/appkaodkolejki/AppkaOdKolejkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.appkaodkolejki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppkaOdKolejkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppkaOdKolejkiApplication.class, args);
+	}
+
+}
---
+++ appkaOdKolejki/src/test/java/com/example/appkaodkolejki/AppkaOdKolejkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appkaodkolejki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppkaOdKolejkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
